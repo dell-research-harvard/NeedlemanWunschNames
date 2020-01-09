7671cf54d213132d7e63f94ca2a81ec4de29a51e
Ed Jee <edjee96@gmail.com>
2020-01-09 11:16:59 -0500

    moved arg parsing

--- align-company-names.py
+++ align-company-names.py
@@ -6,8 +6,12 @@ import regex
 import argparse
 from NW_Company_Names.NW_company_names import create_nw_df 
 
-
-
+# Arg parsing
+parser = argparse.ArgumentParser(description="Match sequences of company names.")
+parser.add_argument("index_csv", type = argparse.FileType("r"), help = "CSV with index name data")
+parser.add_argument("book_csv", type = argparse.FileType("r"), help = "CSV with company name data")
+parser.add_argument("output_csv", type = argparse.FileType("w"), help = "Output name")
+parser.add_argument("--data_source_type", type = str, help = "data source type (defaults to firm)", default="firm")
 
 
 def NW_align_names(index_csv, book_csv, output_csv, data_source_type = "firm"):
@@ -30,7 +34,6 @@ def NW_align_names(index_csv, book_csv, output_csv, data_source_type = "firm"):
     """
     index_df = pd.read_csv(index_csv)
     book_df = pd.read_csv(book_csv)
-
     subset_index_df = index_df[index_df["data_source_type"] == data_source_type]
     subset_book_df = book_df[book_df["companyid"].str.contains(data_source_type)]
 
@@ -47,7 +50,6 @@ def NW_align_names(index_csv, book_csv, output_csv, data_source_type = "firm"):
     
     # Arranging by companyid MUST BE ZERO PADDED OR WON'T BE IN ORDER
     subset_book_df = subset_book_df.sort_values(by = ["companyid"])
-
     # Run algo
     matched_df =  create_nw_df(
         subset_book_df,
@@ -60,14 +62,7 @@ def NW_align_names(index_csv, book_csv, output_csv, data_source_type = "firm"):
 
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="Match sequences of company names.")
-    parser.add_argument("index_csv", type = argparse.FileType("r"), help = "CSV with index name data")
-    parser.add_argument("book_csv", type = argparse.FileType("r"), help = "CSV with company name data")
-    parser.add_argument("output_csv", type = argparse.FileType("w"), help = "Output name")
-    parser.add_argument("--data_source_type", type = str, help = "data source type (defaults to firm)", default="firm")
     args = parser.parse_args()
-
     # Timing
     start = time.time()
     init_df = NW_align_names(
