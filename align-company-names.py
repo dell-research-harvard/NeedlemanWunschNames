e6da0360d331c42129291c11d29f8fd208ab5bf1
Ed Jee <edjee96@gmail.com>
2020-01-16 15:30:13 -0500

    added back data source arg as splitting in R seems a little impractical atm

--- align-company-names.py
+++ align-company-names.py
@@ -15,13 +15,14 @@ parser.add_argument("--nrows", type = int, help = "nrow to use", default=None)
 parser.add_argument("--gap_penalty", type = float, help = "gap penalty size", default=-1)
 parser.add_argument("--index_col", type = str, help = "index text column name", default="index_text")
 parser.add_argument("--book_col", type = str, help = "book text column name", default="book_text")
-
+parser.add_argument("--data_source_type", type = str, help = "Firm type used in PR (bank, credit union, etc.)", default=None)
 
 def NW_align_names(index_csv,
                    book_csv,
                    output_csv,
                    index_col,
                    book_col,
+                   data_source_type = None,
                    nrows = None,
                    gap_penalty = -1):
     """
@@ -45,6 +46,21 @@ def NW_align_names(index_csv,
     """
     index_df = pd.read_csv(index_csv,   nrows = nrows)
     book_df = pd.read_csv(book_csv, nrows = nrows)    
+    
+    
+    # Since Personnel Records uses data source type to split up sections of the book
+    # we don't want to match credit_union firms to bank firms.
+    # Might be nicer to refactor this or split data in R before it reaches this stage
+    if data_source_type != None:
+        index_df = index_df[index_df["data_source_type"] == data_source_type]
+        book_df = book_df[book_df["companyid"].str.contains(data_source_type)]
+        # Again this is ugly and should be done elsewhere.
+        # We want to ensure company names are ordered sequentially as they appear
+        # in the book - as long as companyid is ZERO PADDED this will order entries
+
+        # This should be the case for 54 (and hopefully 56) but here's a quick doublecheck
+        book_df = book_df.sort_values(by = ["companyid"])
+
 
     subset_index_df = index_df.reset_index()
     subset_book_df = book_df.reset_index()
@@ -57,8 +73,6 @@ def NW_align_names(index_csv,
     subset_index_df["clean_index_text"] = subset_index_df["clean_index_text"].apply(lambda x: regex.sub(r"\p{Punct}", u"", str(x)))
     subset_index_df["clean_index_text"] = subset_index_df["clean_index_text"].apply(lambda x: regex.sub(r"\|", u"", str(x)))
     
-    # Arranging by companyid MUST BE ZERO PADDED OR WON'T BE IN ORDER
-    # subset_book_df = subset_book_df.sort_values(by = ["companyid"])
     # Run algo
     matched_df =  create_nw_df(
         subset_book_df,
@@ -81,6 +95,7 @@ if __name__ == "__main__":
         args.output_csv,
         index_col = args.index_col,
         book_col = args.book_col,
+        data_source_type = args.data_source_type,
         nrows = args.nrows,
         gap_penalty = args.gap_penalty
     )
